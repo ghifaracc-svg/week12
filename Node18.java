7cb03f7d68c1672ef0fce184b6c7c8d798fec5c0
Muhammad Abu Dzar Al Ghifari <ghifaracc@gmail.com>
2026-05-08 16:34:09 +0700

    1

---
+++ Node18.java
@@ -0,0 +1,10 @@
+public class Node18 {
+    Student18 data;
+    Node18 next;
+    public Node18(){
+    }
+    public Node18(Student18 data, Node18 next){
+    this.data = data;
+    this.next = next;
+    }
+}
---
+++ Student18.java
@@ -0,0 +1,20 @@
+package week12;
+
+public class Student18 {
+    String nim, name, className;
+    double gpa;
+
+    public Student18() {
+    }
+
+    public Student18(String nm, String name, String kls, double ip) {
+        this.nim = nm;
+        this.name = name;
+        this.className = kls;
+        this.gpa = ip;
+    }
+
+    void print() {
+        System.out.println(nim + " - " + name + " - " + className + " - " + gpa);
+    }
+}
